615135108fd44f9b793e5ac8bc82ada8fbfac2d8
Itechnorove <itechnorove@gmail.com>
2020-08-20 00:38:59 +0530

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent} from './home/home.component'
+import {LoginComponent} from './account/login/login.component';
+import { SignupComponent } from './account/signup/signup.component';
+import { ForgotComponent } from './account/forgot/forgot.component';
+import { AddressComponent } from './account/address/address.component';
+import { ProfileComponent } from './account/profile/profile.component';
+import { CityComponent } from './account/city/city.component';
+import { VerifyComponent } from './account/verify/verify.component';
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'login', component:LoginComponent},
+  {path:'signup', component:SignupComponent},
+  {path:'forgot', component:ForgotComponent},
+  {path:'address', component:AddressComponent},
+  {path:'profile', component:ProfileComponent},
+  {path:'city', component:CityComponent},
+  {path:'verify',component:VerifyComponent}
 
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { CategorydetailsComponent } from './categorydetails/categorydetails.component';
+import { CityComponent } from './account/city/city.component';
+import { ForgotComponent } from './account/forgot/forgot.component';
+import { LoginComponent } from './account/login/login.component';
+import { SignupComponent } from './account/signup/signup.component';
+import { VerifyComponent } from './account/verify/verify.component';
+import { AddressComponent } from './account/address/address.component';
+import { ProfileComponent } from './account/profile/profile.component';
+import { OrderComponent } from './account/order/order.component';
+import { HeaderComponent } from './Component/header/header.component';
+import { FooterComponent } from './Component/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    CategorydetailsComponent,
+    CityComponent,
+    ForgotComponent,
+    LoginComponent,
+    SignupComponent,
+    VerifyComponent,
+    AddressComponent,
+    ProfileComponent,
+    OrderComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
